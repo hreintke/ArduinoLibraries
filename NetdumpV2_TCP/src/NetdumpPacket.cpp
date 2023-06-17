764ab77d7692f3757cfa889959a7a90ad4121d02
hreintke <hreintke@tauri.nl>
2023-06-17 15:16:42 +0200

    Add Netdump V2 library

---
+++ NetdumpV2_TCP/src/NetdumpPacket.cpp
@@ -0,0 +1,433 @@
+/*
+    NetDump library - tcpdump-like packet logger facility
+
+    Copyright (c) 2018 David Gauchard. All rights reserved.
+    This file is part of the esp8266 core for Arduino environment.
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "Netdump.h"
+#include <lwip/init.h>
+
+namespace NetCapture
+{
+
+void Packet::printDetail(Print& out, const String& indent, const uint8_t* data, size_t size, PacketDetail pd) const
+{
+    if (pd == PacketDetail::NONE)
+    {
+        return;
+    }
+
+    uint16_t charCount = (pd == PacketDetail::CHAR) ? 80 : 24;
+
+    size_t start = 0;
+    while (start < size)
+    {
+        size_t end = start + charCount;
+        if (end > size)
+        {
+            end = size;
+        }
+        out.printf("%s", indent.c_str());
+        if (pd != PacketDetail::CHAR)
+        {
+            for (size_t i = start; i < end; i++)
+            {
+                out.printf("%02x ", (unsigned char)data[i]);
+            }
+            for (size_t i = end; i < start + charCount; i++)
+            {
+                out.print("   ");
+            }
+        }
+        for (size_t i = start; i < end; i++)
+        {
+            out.printf("%c", data[i] >= 32 && data[i] < 128 ? data[i] : '.');
+        }
+        out.println();
+
+        start += charCount;
+    }
+}
+
+void Packet::setPacketType(PacketType pt)
+{
+    thisPacketType = pt;
+    thisAllPacketTypes.emplace_back(pt);
+}
+
+void Packet::setPacketTypes()
+{
+    if (isARP())
+    {
+        setPacketType(PacketType::ARP);
+    }
+    else if (isIP())
+    {
+        setPacketType(PacketType::IP);
+        setPacketType(isIPv4() ? PacketType::IPv4 : PacketType::IPv6);
+        if (isUDP())
+        {
+            setPacketType(PacketType::UDP);
+            if (isMDNS())
+            {
+                setPacketType(PacketType::MDNS);
+            }
+            if (isDNS())
+            {
+                setPacketType(PacketType::DNS);
+            }
+            if (isLLMNR())
+            {
+            	setPacketType(PacketType::LLMNR);
+            }
+            if (isSSDP())
+            {
+                setPacketType(PacketType::SSDP);
+            }
+            if (isDHCP())
+            {
+                setPacketType(PacketType::DHCP);
+            }
+            if (isWSDD())
+            {
+                setPacketType(PacketType::WSDD);
+            }
+            if (isNETBIOS())
+            {
+                setPacketType(PacketType::NETBIOS);
+            }
+            if (isSMB())
+            {
+                setPacketType(PacketType::SMB);
+            }
+            if (isOTA())
+            {
+                setPacketType(PacketType::OTA);
+            }
+        }
+        if (isTCP())
+        {
+            setPacketType(PacketType::TCP);
+            if (isHTTP())
+            {
+                setPacketType(PacketType::HTTP);
+            }
+        }
+        if (isICMP())
+        {
+            setPacketType(PacketType::ICMP);
+        }
+        if (isIGMP())
+        {
+            setPacketType(PacketType::IGMP);
+        }
+    }
+    else
+    {
+        setPacketType(PacketType::UKNW);
+    }
+}
+
+const PacketType Packet::packetType() const
+{
+    return thisPacketType;
+}
+
+const std::vector<PacketType>& Packet::allPacketTypes() const
+{
+    return thisAllPacketTypes;
+}
+
+void Packet::MACtoString(const uint8_t* mac, StreamString& sstr) const
+{
+    for (int i = 0; i < 6; i++)
+    {
+        sstr.printf_P(PSTR("%02x"), mac[i]);
+        if (i < 5)
+        {
+            sstr.print(':');
+        }
+    }
+}
+
+void Packet::ARPtoString(PacketDetail netdumpDetail, StreamString& sstr) const
+{
+	if (!arpPacket)
+	{
+		sstr.printf_P(PSTR("ARPtoString access error\r\n"));
+		return;
+	}
+    switch (arpPacket->opcode())
+    {
+    case 1 : sstr.printf_P(PSTR("who has %s tell %s"), arpPacket->targetIP().toString().c_str(), arpPacket->senderIP().toString().c_str());
+        break;
+    case 2 : sstr.printf_P(PSTR("%s is at "), arpPacket->senderIP().toString().c_str());
+        MACtoString(arpPacket->hdr->senderHardwareAddress, sstr);
+        break;
+    }
+    sstr.printf("\r\n");
+    printDetail(sstr, PSTR("           D "), arpPacket->raw, packetLength - ETH_HDR_LEN, netdumpDetail);
+}
+
+void Packet::DNStoString(PacketDetail netdumpDetail, StreamString& sstr) const
+{
+	if (!dnsPacket || !udpPacket || !ipPacket)
+	{
+		sstr.printf_P(PSTR("DNStoString access error\r\n"));
+		return;
+	}
+	sstr.printf_P(PSTR("%s>%s "),  ipPacket->sourceIP().toString().c_str(), ipPacket->destinationIP().toString().c_str());
+    sstr.printf_P(PSTR("ID=0x%04x "), dnsPacket->id());
+    sstr.printf_P(PSTR("F=0x%04x "), dnsPacket->flags());
+
+    if (uint16_t t = dnsPacket->qdcount())
+    {
+        sstr.printf_P(PSTR("Q=%d "), t);
+    }
+    if (uint16_t t = dnsPacket->ancount())
+    {
+        sstr.printf_P(PSTR("R=%d "), t);
+    }
+    if (uint16_t t = dnsPacket->nscount())
+    {
+        sstr.printf_P(PSTR("TR=%d "), t);
+    }
+    if (uint16_t t = dnsPacket->arcount())
+    {
+        sstr.printf_P(PSTR("DR=%d "), t);
+    }
+    sstr.printf_P(PSTR("\r\n"));
+    for (int i = 0;i<dnsPacket->qdcount();i++)
+	  {
+		  DNSPacket::DNSQuestion dq = dnsPacket->getQuestion(i);
+		  sstr.printf("               Q : %s, type %d\r\n",dq.qname.c_str(),dq.qtype);
+	  }
+	  for (int i=0;i<dnsPacket->ancount();i++)
+	  {
+		  DNSPacket::DNSAnswer da = dnsPacket->getAnswer(i);
+		  sstr.printf("               R : %s TP : %d TTL : %d",da.name.c_str(),da.type,da.ttl);
+		  if (da.getIP().isSet())
+		  {
+			  sstr.printf(" IP : %s\r\n",da.getIP().toString().c_str());
+		  }
+		  else
+		  {
+			  sstr.printf("\r\n");
+		  }
+	  }
+    printDetail(sstr, PSTR("           H "), udpPacket->raw, udpPacket->hdrLength(), netdumpDetail);
+    printDetail(sstr, PSTR("           D "), udpPacket->hdr->payload, udpPacket->length(), netdumpDetail);
+}
+
+void Packet::UDPtoString(PacketDetail netdumpDetail, StreamString& sstr) const
+{
+    sstr.printf_P(PSTR("%s>%s "), sourceIP().toString().c_str(), destIP().toString().c_str());
+    sstr.printf_P(PSTR("%d:%d"), getSrcPort(), getDstPort());
+    sstr.printf_P(PSTR("\r\n"));
+    printDetail(sstr, PSTR("           H "), udpPacket->raw, getUdpHdrLen(), netdumpDetail);
+    printDetail(sstr, PSTR("           D "), udpPacket->hdr->payload, getUdpLen(), netdumpDetail);
+}
+
+void Packet::TCPtoString(PacketDetail netdumpDetail, StreamString& sstr) const
+{
+	if (!tcpPacket)
+	{
+		sstr.printf_P(PSTR("TCPtoString access error\r\n"));
+		return;
+	}
+    sstr.printf_P(PSTR("%s>%s "), sourceIP().toString().c_str(), destIP().toString().c_str());
+    sstr.printf_P(PSTR("%d:%d "), getSrcPort(), getDstPort());
+    uint16_t flags = getTcpFlags();
+    sstr.print('[');
+    const char chars [] = "FSRPAUECN";
+    for (uint8_t i = 0; i < sizeof chars; i++)
+        if (flags & (1 << i))
+        {
+            sstr.print(chars[i]);
+        }
+    sstr.print(']');
+    sstr.printf_P(PSTR(" len: %u seq: %u, ack: %u, wnd: %u "), getTcpLen(), getTcpSeq(), getTcpAck(), getTcpWindow());
+    sstr.printf_P(PSTR("\r\n"));
+    printDetail(sstr, PSTR("           H "), tcpPacket->raw, getTcpHdrLen(), netdumpDetail);
+    printDetail(sstr, PSTR("           D "), tcpPacket->hdr->payload, getTcpLen(), netdumpDetail);
+}
+
+void Packet::ICMPtoString(PacketDetail netdumpDetail, StreamString& sstr) const
+{
+	if (!icmpPacket || !ipPacket)
+	{
+		sstr.printf_P(PSTR("ICMPtoString access error icmp\r\n"));
+		return;
+	}
+	if (!ipPacket)
+	{
+		sstr.printf_P(PSTR("ICMPtoString access error ip\r\n"));
+		return;
+	}
+
+	sstr.printf_P(PSTR("%s>%s "), sourceIP().toString().c_str(), destIP().toString().c_str());
+    if (isIPv4())
+    {
+        switch (getIcmpType())
+        {
+        case 0 : sstr.printf_P(PSTR("ping reply")); break;
+        case 3 : sstr.printf_P(PSTR("destination unreachable")); break;
+        case 5 : sstr.printf_P(PSTR("redirect")); break;
+        case 8 : sstr.printf_P(PSTR("ping request")); break;
+
+        default: sstr.printf_P(PSTR("type(0x%02x)"), getIcmpType()); break;
+        }
+    }
+    if (isIPv6())
+    {
+        switch (getIcmpType())
+        {
+        case 129 : sstr.printf_P(PSTR("ping reply")); break;
+        case 128 : sstr.printf_P(PSTR("ping request")); break;
+        case 135 : sstr.printf_P(PSTR("Neighbour solicitation")); break;
+        case 136 : sstr.printf_P(PSTR("Neighbour advertisement")); break;
+        default: sstr.printf_P(PSTR("type(0x%02x)"), getIcmpType()); break;
+        }
+    }
+    sstr.printf("\r\n");
+}
+
+void Packet::IGMPtoString(PacketDetail netdumpDetail, StreamString& sstr) const
+{
+    switch (getIgmpType())
+    {
+    case 1 : sstr.printf_P(PSTR("Create Group Request")); break;
+    case 2 : sstr.printf_P(PSTR("Create Group Reply")); break;
+    case 3 : sstr.printf_P(PSTR("Join Group Request")); break;
+    case 4 : sstr.printf_P(PSTR("Join Group Reply")); break;
+    case 5 : sstr.printf_P(PSTR("Leave Group Request")); break;
+    case 6 : sstr.printf_P(PSTR("Leave Group Reply")); break;
+    case 7 : sstr.printf_P(PSTR("Confirm Group Request")); break;
+    case 8 : sstr.printf_P(PSTR("Confirm Group Reply")); break;
+    case 0x11 : sstr.printf_P(PSTR("Group Membership Query")); break;
+    case 0x12 : sstr.printf_P(PSTR("IGMPv1 Membership Report")); break;
+    case 0x22 : sstr.printf_P(PSTR("IGMPv3 Membership Report")); break;
+    default: sstr.printf_P(PSTR("type(0x%02x)"), getIgmpType()); break;
+    }
+    sstr.printf_P(PSTR("\r\n"));
+}
+
+void Packet::IPtoString(PacketDetail netdumpDetail, StreamString& sstr) const
+{
+    sstr.printf_P(PSTR("%s>%s "), sourceIP().toString().c_str(), destIP().toString().c_str());
+    sstr.printf_P(PSTR("Unknown IP type : %d\r\n"), ipType());
+    printDetail(sstr, PSTR("           H "), ipPacket->raw(), getIpHdrLen(), netdumpDetail);
+    printDetail(sstr, PSTR("           D "), ipPacket->raw(), getIpTotalLen() - getIpHdrLen(), netdumpDetail);
+}
+
+void Packet::UKNWtoString(PacketDetail netdumpDetail, StreamString& sstr) const
+{
+    sstr.printf_P(PSTR("Unknown EtherType 0x%04x Src : "), ethType());
+    MACtoString(rawData(), sstr);
+    sstr.printf_P(PSTR(" Dst : "));
+    MACtoString(rawData()+6, sstr);
+    sstr.printf_P(PSTR("\r\n"));
+}
+
+const String Packet::toString() const
+{
+    return toString(PacketDetail::NONE);
+}
+
+
+const String Packet::toString(PacketDetail netdumpDetail) const
+{
+    StreamString sstr;
+    sstr.reserve(128);
+
+    sstr.printf_P(PSTR("%d %3s %-4s "), netif_idx, out ? "out" : "in ", packetType().toString().c_str());
+
+    if (netdumpDetail == PacketDetail::RAW)
+    {
+        sstr.printf_P(PSTR(" : "));
+        for (auto at : thisAllPacketTypes)
+        {
+            sstr.printf_P(PSTR("%s "), at.toString().c_str());
+        }
+        sstr.printf_P(PSTR("\r\n"));
+        printDetail(sstr, PSTR("           D "), rawData(), packetLength, netdumpDetail);
+        return sstr;
+    }
+
+    switch (thisPacketType)
+    {
+    case PacketType::ARP :
+    {
+        ARPtoString(netdumpDetail, sstr);
+        break;
+    }
+    case PacketType::MDNS :
+    case PacketType::DNS :
+    case PacketType::LLMNR:
+    {
+        DNStoString(netdumpDetail, sstr);
+        break;
+    }
+    case PacketType::SSDP :
+    case PacketType::DHCP :
+    case PacketType::WSDD :
+    case PacketType::NETBIOS :
+    case PacketType::SMB :
+    case PacketType::OTA :
+    case PacketType::UDP :
+    {
+        UDPtoString(netdumpDetail, sstr);
+        break;
+    }
+    case PacketType::TCP :
+    case PacketType::HTTP :
+    {
+        TCPtoString(netdumpDetail, sstr);
+        break;
+    }
+    case PacketType::ICMP :
+    {
+        ICMPtoString(netdumpDetail, sstr);
+        break;
+    }
+    case PacketType::IGMP :
+    {
+        IGMPtoString(netdumpDetail, sstr);
+        break;
+    }
+    case PacketType::IPv4 :
+    case PacketType::IPv6 :
+    {
+        IPtoString(netdumpDetail, sstr);
+        break;
+    }
+    case PacketType::UKNW :
+    {
+        UKNWtoString(netdumpDetail, sstr);
+        break;
+    }
+    default :
+    {
+        sstr.printf_P(PSTR("Non identified packet\r\n"));
+        break;
+    }
+    }
+    return sstr;
+}
+
+} // namespace NetCapture
---
+++ NetdumpV2_TCP/src/NetdumpPacket.h
@@ -0,0 +1,392 @@
+/*
+    NetDump library - tcpdump-like packet logger facility
+
+    Copyright (c) 2019 Herman Reintke. All rights reserved.
+    This file is part of the esp8266 core for Arduino environment.
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef __NETDUMP_PACKET_H
+#define __NETDUMP_PACKET_H
+
+#include <lwipopts.h>
+#include <IPAddress.h>
+#include <StreamString.h>
+#include "NetdumpIP.h"
+#include "PacketType.h"
+#include <vector>
+#include "NetdumpUtils.h"
+#include "structures.h"
+#include <memory>
+namespace NetCapture
+{
+
+class Packet
+{
+public:
+	static int constexpr ETH_HDR_LEN = 14;
+
+    Packet(unsigned long msec, int n, const char* d, size_t l, int o, int s)
+        : packetTime(msec), netif_idx(n), data(d), packetLength(l), out(o), success(s)
+    {
+        setPacketTypes();
+//        ethernetFrame = new EthernetFrame(rawData());
+        ethernetFrame.reset(new EthernetFrame(rawData()));
+//        ethernetFrame = std::unique_ptr<EthernetFrame>(new EthernetFrame(rawData()));
+        switch (ethernetFrame->type())
+        {
+        case 0x0800 :
+//        	ipv4Packet = new IPv4Packet(ethernetFrame->hdr->payload);
+//        	ipPacket = new IPPacket(ipv4Packet);
+ //       	ipv4Packet->reset(new IPv4Packet(ethernetFrame->hdr->payload));
+        	ipv4Packet.reset(new IPv4Packet(ethernetFrame->hdr->payload));
+        	ipPacket.reset(new IPPacket(ipv4Packet));
+        	break;
+        case 0x86dd :
+//        	ipv6Packet = new IPv6Packet(ethernetFrame->hdr->payload);
+//        	ipPacket = new IPPacket(ipv6Packet);
+        	ipv6Packet.reset(new IPv6Packet(ethernetFrame->hdr->payload));
+        	ipPacket.reset(new IPPacket(ipv6Packet));
+        	break;
+        case 0x0806 :
+//			arpPacket = new ARPPacket(ethernetFrame->hdr->payload);
+        	arpPacket.reset(new ARPPacket(ethernetFrame->hdr->payload));
+        	break;
+        default :
+        	break;
+        }
+
+        if (ipPacket)
+        {
+        	switch (ipPacket->packetType())
+			{
+        	case 17 : //udpPacket = new UDPPacket(ipPacket->payload());
+        			  udpPacket.reset(new UDPPacket(ipPacket->payload()));
+        			  break;
+        	case 6  : //tcpPacket = new TCPPacket(ipPacket->payload());
+        			  tcpPacket.reset(new TCPPacket(ipPacket->payload()));
+        			  break;
+        	case 1	:
+        	case 58 :
+        			  //icmpPacket = new ICMPPacket(ipPacket->payload());
+        			  icmpPacket.reset(new ICMPPacket(ipPacket->payload()));
+        			  break;
+        	default : break;
+			}
+        }
+        if (udpPacket)
+        {
+        	if (((udpPacket->sourcePort() == 5353) || (udpPacket->destinationPort() == 5353))
+        		|| ((udpPacket->sourcePort() == 53) || (udpPacket->destinationPort() == 53))
+				|| ((udpPacket->sourcePort() == 5355) || (udpPacket->destinationPort() == 5355)))
+        	{
+//        		dnsPacket = new DNSPacket(udpPacket->hdr->payload);
+        		dnsPacket.reset(new DNSPacket(udpPacket->hdr->payload));
+        	}
+        }
+    };
+
+    Packet() {};
+
+    enum class PacketDetail
+    {
+        NONE,
+        FULL,
+        CHAR,
+        RAW
+    };
+
+    std::unique_ptr<EthernetFrame> ethernetFrame = nullptr;
+//    EthernetFrame* ethernetFrame = nullptr;
+    std::unique_ptr<ARPPacket> arpPacket = nullptr;
+    std::unique_ptr<IPv4Packet> ipv4Packet = nullptr;
+    std::unique_ptr<IPv6Packet> ipv6Packet = nullptr;
+    std::unique_ptr<IPPacket> ipPacket = nullptr;
+    std::unique_ptr<UDPPacket> udpPacket = nullptr;
+    std::unique_ptr<DNSPacket> dnsPacket = nullptr;
+    std::unique_ptr<TCPPacket> tcpPacket = nullptr;
+    std::unique_ptr<ICMPPacket> icmpPacket = nullptr;
+
+
+    const uint8_t* rawData() const
+    {
+        return reinterpret_cast<const uint8_t*>(data);
+    }
+    int getInOut() const
+    {
+        return out;
+    }
+    time_t getTime() const
+    {
+        return packetTime;
+    }
+    uint32_t getPacketSize() const
+    {
+        return packetLength;
+    }
+    uint16_t ntoh16(uint16_t idx) const
+    {
+        return data[idx + 1] | (((uint16_t)data[idx]) << 8);
+    };
+    uint32_t ntoh32(uint16_t idx) const
+    {
+        return ntoh16(idx + 2) | (((uint32_t)ntoh16(idx)) << 16);
+    };
+    uint8_t  byteData(uint16_t idx) const
+    {
+        return data[idx];
+    }
+    const char* byteIdx(uint16_t idx) const
+    {
+        return &data[idx];
+    };
+    uint16_t ethType() const
+    {
+        return ntoh16(12);
+    };
+    uint8_t  ipType() const
+    {
+        return isIP() ? isIPv4() ? data[ETH_HDR_LEN + 9] : data[ETH_HDR_LEN + 6] : 0;
+    };
+    uint16_t getIpHdrLen() const
+    {
+        return isIPv4() ? (((unsigned char)data[ETH_HDR_LEN]) & 0x0f) << 2 : 40 ;   // IPv6 is fixed length
+    }
+    uint16_t getIpTotalLen() const
+    {
+        return isIP() ? isIPv4() ? ntoh16(ETH_HDR_LEN + 2) : (packetLength - ETH_HDR_LEN)   :  0;
+    }
+    uint32_t getTcpSeq() const
+    {
+        return isTCP() ? ntoh32(ETH_HDR_LEN + getIpHdrLen() + 4) : 0;
+    }
+    uint32_t getTcpAck() const
+    {
+        return isTCP() ? ntoh32(ETH_HDR_LEN + getIpHdrLen() + 8) : 0;
+    }
+    uint16_t getTcpFlags() const
+    {
+        return isTCP() ? ntoh16(ETH_HDR_LEN + getIpHdrLen() + 12) : 0;
+    }
+    uint16_t getTcpWindow() const
+    {
+        return isTCP() ? ntoh16(ETH_HDR_LEN + getIpHdrLen() + 14) : 0;
+    }
+    uint8_t  getTcpHdrLen() const
+    {
+        return isTCP() ? (data[ETH_HDR_LEN + getIpHdrLen() + 12] >> 4) * 4 : 0;
+    };//Header len is in multiple of 4 bytes
+    uint16_t getTcpLen() const
+    {
+        return isTCP() ? getIpTotalLen() - getIpHdrLen() - getTcpHdrLen() : 0 ;
+    };
+
+    uint8_t  getIcmpType() const
+    {
+        return icmpPacket ? icmpPacket->hdr->type : 0;
+    }
+    uint8_t  getIgmpType() const
+    {
+        return isIGMP() ? data[ETH_HDR_LEN + getIpHdrLen() + 0] : 0;
+    }
+    uint8_t getARPType() const
+    {
+        return isARP() ? data[ETH_HDR_LEN + 7] : 0;
+    }
+    bool    is_ARP_who() const
+    {
+        return (getARPType() == 1);
+    }
+    bool    is_ARP_is() const
+    {
+        return (getARPType() == 2);
+    }
+
+    uint8_t getUdpHdrLen() const
+    {
+        return isUDP() ? 8 : 0;
+    };
+    uint16_t getUdpLen() const
+    {
+        return isUDP() ? ntoh16(ETH_HDR_LEN + getIpHdrLen() + 4) : 0;
+    };
+    bool isARP() const
+    {
+        return (ethType() == 0x0806);
+    };
+    bool isIPv4() const
+    {
+        return (ethType() == 0x0800);
+    };
+    bool isIPv6() const
+    {
+        return (ethType() == 0x86dd);
+    };
+    bool isIP() const
+    {
+        return (isIPv4() || isIPv6());
+    };
+    bool isICMP() const
+    {
+        return (isIP() && ((ipType() == 1) || (ipType() == 58)));
+    };
+    bool isIGMP() const
+    {
+        return (isIP() && (ipType() == 2));
+    };
+    bool isTCP() const
+    {
+        return (isIP() && (ipType() == 6));
+    };
+    bool isUDP() const
+    {
+        return (isIP() && ipType() == 17);
+    };
+    bool isMDNS() const
+    {
+        return (isUDP() && hasPort(5353));
+    };
+    bool isDNS() const
+    {
+        return (isUDP() && hasPort(53));
+    };
+    bool isLLMNR() const
+    {
+    	return (isUDP() && hasPort(5355));
+    }
+    bool isSSDP() const
+    {
+        return (isUDP() && hasPort(1900));
+    };
+    bool isDHCP() const
+    {
+        return (isUDP() && ((hasPort(546) || hasPort(547) || hasPort(67) || hasPort(68))));
+    };
+    bool isWSDD() const
+    {
+        return (isUDP() && hasPort(3702));
+    };
+    bool isHTTP() const
+    {
+        return (isTCP() && hasPort(80));
+    };
+    bool isOTA() const
+    {
+        return (isUDP() && hasPort(8266));
+    }
+    bool isNETBIOS() const
+    {
+        return (isUDP() && (hasPort(137) || hasPort(138) || hasPort(139)));
+    }
+    bool isSMB() const
+    {
+        return (isUDP() && hasPort(445));
+    }
+    NetdumpIP getIP(uint16_t idx) const
+    {
+        return NetdumpIP(data[idx], data[idx + 1], data[idx + 2], data[idx + 3]);
+    };
+
+    NetdumpIP getIP6(uint16_t idx) const
+    {
+        return NetdumpIP((const uint8_t*)&data[idx], false);
+    };
+    NetdumpIP sourceIP() const
+    {
+    	return ipPacket ? ipPacket->sourceIP() : NetdumpIP();
+        NetdumpIP ip;
+        if (isIPv4())
+        {
+            ip = getIP(ETH_HDR_LEN + 12);
+        }
+        else if (isIPv6())
+        {
+            ip = getIP6(ETH_HDR_LEN + 8);
+        }
+        return ip;
+    };
+
+    bool      hasIP(NetdumpIP ip) const
+    {
+        return (isIP() && ((ip == sourceIP()) || (ip == destIP())));
+    }
+
+    NetdumpIP destIP() const
+    {
+    	return ipPacket ? ipPacket->destinationIP() : NetdumpIP();
+        NetdumpIP ip;
+        if (isIPv4())
+        {
+            ip = getIP(ETH_HDR_LEN + 16);
+        }
+        else if (isIPv6())
+        {
+            ip = getIP6(ETH_HDR_LEN + 24);
+        }
+        return ip;
+    };
+    uint16_t getSrcPort() const
+    {
+ //   	return tcpPacket ? tcpPacket->sourcePort() : (udpPacket ? udpPacket->sourcePort() : 0);
+        return isIP() ? ntoh16(ETH_HDR_LEN + getIpHdrLen() + 0) : 0;
+    }
+    uint16_t getDstPort() const
+    {
+   // 	return tcpPacket ? tcpPacket->destinationPort() : (udpPacket ? udpPacket->destinationPort() : 0);
+        return isIP() ? ntoh16(ETH_HDR_LEN + getIpHdrLen() + 2) : 0;
+    }
+    bool     hasPort(uint16_t p) const
+    {
+        return (isIP() && ((getSrcPort() == p) || (getDstPort() == p)));
+    }
+
+    const String toString() const;
+    const String toString(PacketDetail netdumpDetail) const;
+    void printDetail(Print& out, const String& indent, const uint8_t* data, size_t size, PacketDetail pd) const;
+
+    const PacketType packetType() const;
+    const std::vector<PacketType>& allPacketTypes() const;
+
+
+private:
+
+    void setPacketType(PacketType);
+    void setPacketTypes();
+
+    void MACtoString(const uint8_t* mac, StreamString& sstr) const;
+    void ARPtoString(PacketDetail netdumpDetail, StreamString& sstr) const;
+    void DNStoString(PacketDetail netdumpDetail, StreamString& sstr) const;
+    void UDPtoString(PacketDetail netdumpDetail, StreamString& sstr) const;
+    void TCPtoString(PacketDetail netdumpDetail, StreamString& sstr) const;
+    void ICMPtoString(PacketDetail netdumpDetail, StreamString& sstr) const;
+    void IGMPtoString(PacketDetail netdumpDetail, StreamString& sstr) const;
+    void IPtoString(PacketDetail netdumpDetail, StreamString& sstr) const;
+    void UKNWtoString(PacketDetail netdumpDetail, StreamString& sstr) const;
+
+
+    time_t packetTime;
+    int netif_idx;
+    const char* data;
+    size_t packetLength;
+    int out;
+    int success;
+    PacketType thisPacketType;
+    std::vector<PacketType> thisAllPacketTypes;
+};
+
+} // namespace NetCapture
+
+#endif /* __NETDUMP_PACKET_H */
